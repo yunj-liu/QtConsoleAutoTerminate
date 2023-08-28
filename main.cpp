3409cc350b27e54ecd6af8e4f93b7ea5a9c2af35
lyj-Desktop <liu.yunjie@qq.com>
2023-08-28 11:14:17 +0800

    2nd commit

---
+++ main.cpp
@@ -0,0 +1,21 @@
+﻿#include <QCoreApplication>
+#include <iostream>
+#include <QTimer>
+//using namespace std;
+using std::cout, std::endl;
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+
+    QString str = "Hello";
+    cout << "abc123" << endl;
+    cout << str.toStdString().data() << endl;
+
+    //Add this, otherwise the Qt console program will not end automatically
+    QTimer t;
+    t.connect(&t, SIGNAL(timeout()), &a, SLOT(quit()));
+    t.start(20);
+
+    return a.exec();
+}
